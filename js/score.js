20ce83bcf5c9867dd5669e32f712eb224f780f3f
Zek0o_ <gabcouston@gmail.com>
2025-08-30 02:39:32 +0000

    Update score.js

--- js/score.js
+++ js/score.js
@@ -24,9 +24,9 @@ export function score(rank, percent, minPercent) {
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
     */
     // New formula
-    let score = (-24.9975*Math.pow(rank-1, 0.4) + 200) *
+    let score = (-24.9975*Math.pow(rank-1, 0.4) + 150) *
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
-
+    
     score = Math.max(0, score);
 
     if (percent != 100) {
